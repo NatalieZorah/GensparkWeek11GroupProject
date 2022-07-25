b80243cd1ef9dcd324d2b8b33b7a414e5726099a
QuyenHuynh <quyenh@hotmail.com>
2022-07-25 11:20:09 -0400

    cart css in process

--- pucci_site/src/components/Bag/Bag.js
+++ pucci_site/src/components/Bag/Bag.js
@@ -11,27 +11,31 @@ export default function Bag(props) {
 	const displayCart = cart.map((product, index) => {
 		subtotal += parseInt(product.price, 10)
 		return <div className='bag-product' key={index}>
-					<div>
-						{product.title}
-					</div>
-					<div>
-						Price: ${product.price}
-					</div>
-				</div>
+			<div>
+				{product.title}
+			</div>
+			<div>
+				Price: ${product.price}
+			</div>
+		</div>
 	})
 
-  return (
-	<>
-		<div className='bag-title'>
-			<h1>Shopping Bag</h1>
-		</div>
-		<div className="bag-container">
-			
-			<div className='bag-cart'>{displayCart}</div>
-			<div className='bag-subtotal'>Subtotal ${subtotal}</div>
-			<button className='btn btn-success'>Checkout</button>
-		</div>
-	</>
-  )
+	return (
+		<>
+			<div className="bag-container">
+
+				<div className='bag-title'>
+					<div className="hero-image">
+						<div className="hero-text">
+							<h1 className="cart-text">Shopping Bag</h1>
+						</div>
+					</div>
+				</div>
+				<div className='bag-cart'>{displayCart}</div>
+				<div className='bag-subtotal'>Subtotal ${subtotal}</div>
+				<button className='btn btn-success'>Checkout</button>
+			</div>
+		</>
+	)
 }
 
