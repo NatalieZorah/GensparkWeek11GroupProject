4976503f58d5c2b769f2c19a5fdc19c7a912d4dd
Natalie Evans <NatalieRose93019@gmail.com>
2022-07-24 22:52:29 -0500

    animations added and styling changes/tweaks
    - added simple animations to various elements
    - general css cleanup as well as some light JS cleanup

--- pucci_site/src/components/Adidogs/Adidogs.js
+++ pucci_site/src/components/Adidogs/Adidogs.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ProductCard from "../ProductCard/ProductCard";
 import "./Adidogs.css";
+import PinkShoes from "./../../assets/images/products/pink-coat-shizu.jpg";
+import RedShoes from "./../../assets/images/products/red-shoe-doggo.jpg";
 
 const products = [
   { id: 1, title: "Jacket", description: "Plaid jacket" },
@@ -16,11 +18,22 @@ const products = [
 const Adidogs = () => {
   return (
     <div className="adidogs-container">
-      {products.map((product) => (
-        <span className="grid-col-span-2">
-          <ProductCard key={product.id}></ProductCard>
-        </span>
-      ))}
+      {/* <div className="split-image-diagonal-l-r grid-col-span-8">
+        <div className="split-image-diagonal-l">
+          <img src={PinkShoes} />
+        </div>
+        <div className="split-image-diagonal-r">
+          <img src={RedShoes} />
+        </div>
+      </div> */}
+
+      <div className="adidogs-card-container">
+        {products.map((product) => (
+          <span key={product.id} className="grid-col-span-2">
+            <ProductCard CardImage="https://dummyimage.com/300" />
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
